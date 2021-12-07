5031ef5274ba35f43b142a565abb591ba1489aa9
Denis Druhakou <denis16.05.2004@icloud.com>
2021-12-08 00:24:10 +0300

    last last last update

--- getMul.js
+++ getMul.js
@@ -1,11 +1,4 @@
 "use strict"
-
-<<<<<<< HEAD
 function getMul( a, b ) { 1
     let mul = a*b ;   // создаём переменную mul и присваиваем ей произведение a и b 
-=======
-function getMul( a, b ) {
-    let mul = a * b;   // создаём переменную mul и присваиваем ей произведение a и b 
-    document.write("Mul(a, b) = " + mul);
->>>>>>> 36ac1c3e6b289df04035a0b9bd8b3fed4cfa9ff8
 }
